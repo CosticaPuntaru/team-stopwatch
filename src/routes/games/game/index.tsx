ec703d727954ccf9f9eee0cf9c35d0e9a2e118cd
costica.puntaru <costica.puntaru@vitals.co>
2023-06-28 22:31:05 +0300

    q

--- src/routes/games/game/index.tsx
+++ src/routes/games/game/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useContext } from '@builder.io/qwik';
+import { component$, useContext, useSignal } from '@builder.io/qwik';
 import { playerStore, timeToHumans } from "~/utils/player-store";
 import { PlayerName } from "~/components/player-name";
 import { getViewTransitionStyle } from "~/utils/transition";
@@ -10,31 +10,60 @@ export default component$(function GamesPage() {
     const gameId = useGameId()
     const selectedGame = gameId && store.games[gameId] || { startTime: 1, players: [] }
     const remainingPlayerCount = selectedGame.players.filter((p) => !p.time).length
+    const filterPlayer = useSignal('')
 
+    // @ts-ignore
     return <div>
         <h1 class="text-1xl font-extrabold dark:text-white">
             Games
             - {gameId && (new Date(parseFloat(gameId))).toDateString()} {remainingPlayerCount > 0 && ' - ' + remainingPlayerCount + ' players remaining'}
         </h1>
-        {/*{!!remainingPlayerCount && (*/}
-        {/*    <div class="text-5xl font-extrabold dark:text-white"><TimePassed startTime={gameId}/></div>*/}
-        {/*)}*/}
+        {!!remainingPlayerCount && (
+            <div class="text-5xl font-extrabold dark:text-white"><TimePassed startTime={gameId}/></div>
+        )}
 
-        {/*{!selectedGame && (*/}
-        {/*    <div>*/}
-        {/*        Failed to find the game*/}
-        {/*    </div>*/}
-        {/*)}*/}
+        {!selectedGame && (
+            <div>
+                Failed to find the game
+            </div>
+        )}
 
         {!!selectedGame && (
             <div class="flex flex-col">
-
                 {!!selectedGame.players.filter((p) => !p.time).length && (
                     <>
-                        <h2 class="text-4xl font-bold dark:text-white m-3">Runners</h2>
+                        <h2 class="text-4xl font-bold dark:text-white my-3">Runners</h2>
+                        <div class="flex flex-col">
+                            <label for="first_name"
+                                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Player
+                                Search
+                            </label>
+                            <input
+                                value={filterPlayer.value}
+                                type="text"
+                                onInput$={(e: any) => filterPlayer.value = (e.target.value)}
+                                onkeydown$={(e: any) => {
+                                    if (e.key === 'Enter') {
+                                        const players = selectedGame.players
+                                            .filter((p) => !p.time && p.name.toLowerCase().includes(filterPlayer.value.toLowerCase()))
+
+                                        if (players.length === 1) {
+                                            const startTime = selectedGame.startTime
+                                            const endTime = Date.now()
+                                            players[0].time = endTime - startTime
+                                        }
+                                    }
+                                }}
+                                id="first_name"
+                                class="mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="John"
+                                required
+                                autocomplete="off"
+                            />
+                        </div>
                         <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
                             {selectedGame.players
-                                .filter((p) => !p.time)
+                                .filter((p) => !p.time && p.name.toLowerCase().includes(filterPlayer.value.toLowerCase()))
                                 .map((player) => (
                                     <div
                                         class={["border-2 rounded-xl p-3 cursor-pointer", {
@@ -76,7 +105,7 @@ export default component$(function GamesPage() {
                     <tbody>
                     {selectedGame.players
                         .filter((p) => p.time)
-                        .sort((a, b) => (a.time ?? 0) - (b.time ?? 0) )
+                        .sort((a, b) => (a.time ?? 0) - (b.time ?? 0))
                         .map((player, index) => (
                             <tr
                                 key={player.name}
